342cea344a2ec648f12f989f499de2f6c6db5e2c
Davíð Örn Jóhannsson <davidoj@siminn.is>
2017-03-02 15:51:39 +0000

    added comment

--- manifests/proxy/log_urls.pp
+++ manifests/proxy/log_urls.pp
@@ -1,4 +1,18 @@
-
+#
+# Configure swift cache_errors.
+#
+# == Dependencies
+#
+# == Examples
+#
+#  include 'swift::proxy::log_urls'
+#
+# == Authors
+#
+#
+# == Copyright
+#
+#
 class swift::proxy::log_urls() {
 
   concat::fragment { 'swift_log_urls':
